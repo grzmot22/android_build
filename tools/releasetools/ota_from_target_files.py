19f97834464424054d7bbae597c01f20b151912c
grzmot22 <grzmot22@gmail.com>
2016-01-10 20:15:51 +0100

    Small changes
    Change-Id: Iae8d478f5b3a1f09e3e344ac7c98266c1a4db613

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -745,15 +745,15 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.AppendExtra('delete_recursive("/system/supersu");')
 # Install ViPER|Atmos Dark 
   script.Print("Extracting ViPER|Atmos Dark...");
-  script.AppendExtra('package_extract_dir("system/V4A", "/tmp/V4A");')
-  script.AppendExtra('run_program("/sbin/busybox", "unzip", "/tmp/V4A/ViPERAtmos_Dark.zip", "META-INF/com/google/android/update-binary", "-d", "/tmp/V4A");')
+  script.AppendExtra('package_extract_dir("system/v4a", "/tmp/v4a");')
+  script.AppendExtra('run_program("/sbin/busybox", "unzip", "/tmp/v4a/v4a.zip", "META-INF/com/google/android/update-binary", "-d", "/tmp/v4a");')
   script.Print("Installing ViPER|Atmos Dark...");
-  script.AppendExtra('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "null", "1", "/tmp/V4A/ViPERAtmos_Dark.zip");')
+  script.AppendExtra('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "null", "1", "/tmp/v4a/v4a.zip");')
 
   script.Print("Cleaning up...");
   script.Mount("/system")
-  script.AppendExtra('delete_recursive("/tmp/V4A");')
-  script.AppendExtra('delete_recursive("/system/V4A");')
+  script.AppendExtra('delete_recursive("/tmp/v4a");')
+  script.AppendExtra('delete_recursive("/system/v4a");')
   script.Unmount("/system");
   script.UnmountAll()
   script.Print("Installation complete!");
